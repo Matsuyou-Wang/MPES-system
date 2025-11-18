453b7edfa43cdd70bd8abcc263a4a2002529bdfd
Matsuyou-Wang <u963167h@ecs.osaka-u.ac.jp>
2025-11-18 17:43:17 +0900

    Force deployment update: add deployment timestamp and update all version numbers to v4.1

---
+++ js/deployment.js
@@ -0,0 +1,8 @@
+/* Deployment timestamp: 2025-11-18 17:30:00 */
+/* This file is used to force cache invalidation */
+
+window.DEPLOYMENT_INFO = {
+    timestamp: '2025-11-18-17-30-00',
+    version: '4.1.0',
+    lastUpdate: 'Major UI improvements: slider fixes, Dominance rename, Empathy redesign'
+};
